cd2224d908ccc62d165eaaf056f1160ffe7696bc
faisalalm01 <75400642+faisalalm01@users.noreply.github.com>
2022-04-15 23:23:05 +0700

    Update booksRoutes.js

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -4,10 +4,10 @@ const booksControllers = require("../controllers/booksControllers");
 // const authMiddleware = require("../helpers/authMiddleware");
 
 booksRoutes.get("/",
-//  authMiddleware.checkLogin, 
+ authMiddleware.checkLogin, 
 booksControllers.getAllBooks);
 booksRoutes.post("/",
-// authMiddleware.checkLogin,
+authMiddleware.checkLogin,
 booksControllers.postBooks);
 booksRoutes.get("/:id",booksControllers.getDataById);
 booksRoutes.delete("/:id",
